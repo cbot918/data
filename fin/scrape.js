cfb39c130c63ef35ab4cd8f4d674a80a8cf6fcfb
cbot918 <cbot918@gmail.com>
2025-02-20 04:31:14 +0800

    update

---
+++ fin/scrape.js
@@ -0,0 +1,7 @@
+
+
+let msftElement = document.querySelector('a[href="/symbols/NASDAQ-MSFT/"]');
+
+let targetElement = document.querySelector(
+    "#js-screener-container > div > div > div.isOverflowHidden-OuXcFHzP > div > div > div.wrapper-TlN8oopI > div > table > tbody > tr:nth-child(3) > td:nth-child(2)"
+);
